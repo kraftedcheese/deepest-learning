329f4739891a428bb318ba7c4dc5679bff19134e
kraftedcheese <38340979+kraftedcheese@users.noreply.github.com>
2022-04-20 15:56:22 +0800

    Clean up data_gen_testing

--- data_gen_testing.py
+++ data_gen_testing.py
@@ -1,118 +1,128 @@
 import config
 import numpy as np
-import os
 import h5py
 import json
-import torch
 
-def data_gen_jsons(voc_list, mode = 'Train', sec_mode = 0):
-    targets_f0_1 = json.load(open("processed_jsons\\f0.json",))
-    targets_f0_1 = np.asarray(targets_f0_1)
-    feats_targs = json.load(open("processed_jsons\\feats_targs.json",))
-    feats_targs = np.asarray(feats_targs)
-    pho_targs = json.load(open("processed_jsons\\phonemes.json",))
-    pho_targs = np.asarray(pho_targs)
-    targets_singers = json.load(open("processed_jsons\\singers.json",))
-    targets_singers = np.asarray(targets_singers)
+# Yields features from test jsons (for testing purposes).
+def data_gen_jsons(file_list, mode="Train", sec_mode=0):
+    target_f0 = json.load(
+        open(
+            "processed_jsons\\target_f0.json",
+        )
+    )
+    target_f0 = np.asarray(target_f0)
+    features = json.load(
+        open(
+            "processed_jsons\\features.json",
+        )
+    )
+    features = np.asarray(features)
+    phonemes = json.load(
+        open(
+            "processed_jsons\\phonemes.json",
+        )
+    )
+    phonemes = np.asarray(phonemes)
+    singers = json.load(
+        open(
+            "processed_jsons\\singers.json",
+        )
+    )
+    singers = np.asarray(singers)
 
-    # yield torch.tensor(feats_targs), torch.tensor(targets_f0_1), torch.tensor(pho_targs), torch.tensor(targets_singers)
-    yield feats_targs, targets_f0_1, np.array(pho_targs), np.array(targets_singers)
+    yield features, target_f0, np.array(phonemes), np.array(singers)
 
-def data_gen(voc_list):
 
-    # print(voc_list)
-
-    stat_file = h5py.File(config.stat_dir+'stats.hdf5', mode='r')
-    max_feat = np.array(stat_file["feats_maximus"])
-    min_feat = np.array(stat_file["feats_minimus"])
-
-    stat_file.close()
-
-    max_files_to_process = int(config.batch_size/config.samples_per_file)
-    # max_files_to_process = 1
+# Yields a batch of samples of a specified length across the dataset for training.
+def data_gen(file_list):
+    # Reads in a statistics file that contains the min and max features over entire dataset.
+    # Min and max features are needed for normalization.
+    stats = h5py.File(config.stat_dir + "stats.hdf5", mode="r")
+    max_f = np.array(stats["feats_maximus"])
+    min_f = np.array(stats["feats_minimus"])
+    stats.close()
 
     num_batches = config.batches_per_epoch_train
-    file_list = voc_list
 
     # The outside number of batches is PAUSED for each iteration (k)
     for k in range(num_batches):
-        feats_targs = []
-        targets_f0_1 = []
-        targets_singers = []
-        pho_targs = []
+        features = []
+        target_f0 = []
+        singers = []
+        phonemes = []
 
-        # maximum of about 5 files? to process
-        while len(feats_targs) < config.batch_size:
-            #randomly choose some file to process
-            voc_index = np.random.randint(0,len(file_list))
-            voc_to_open = file_list[voc_index]
-            voc_file = h5py.File(config.voice_dir + voc_to_open, "r")
-            feats = np.array(voc_file['feats'])
+        # Keep drawing samples if the required batch size has not been met
+        while len(features) < config.batch_size:
+            # Randomly choose some file to process
+            file_index = np.random.randint(0, len(file_list))
+            file_to_open = file_list[file_index]
+            file = h5py.File(config.voice_dir + file_to_open, "r")
+            feats = np.array(file["feats"])
 
-            f0 = feats[:,-2]
-            med = np.median(f0[f0 > 0])
-            f0[f0==0] = med
-            f0_nor = (f0 - min_feat[-2])/(max_feat[-2]-min_feat[-2])
+            # Extract f0 from input features
+            f0 = feats[:, -2]
+            # Fill in missing values with the median
+            median = np.median(f0[f0 > 0])
+            f0[f0 == 0] = median
+            # Normalize based on min and max values
+            f0_normalized = (f0 - min_f[-2]) / (max_f[-2] - min_f[-2])
+            feats = (feats - min_f) / (max_f - min_f)
+            feats[:, -2] = f0_normalized
 
-            feats = (feats-min_feat)/(max_feat-min_feat)
-
-            feats[:,-2] = f0_nor
-
-
-            if voc_to_open.startswith('nus'):
-                if not  "phonemes" in voc_file:
-                    print(voc_file)
-                    Flag = False
-                else: 
-                    Flag = True
-                    pho_target = np.array(voc_file["phonemes"])
-                    singer_name = voc_to_open.split('_')[1]
+            if file_to_open.startswith("nus"):
+                if not "phonemes" in file:
+                    print(file)
+                    has_phonemes = False
+                else:
+                    has_phonemes = True
+                    pho_target = np.array(file["phonemes"])
+                    singer_name = file_to_open.split("_")[1]
                     singer_index = config.singers.index(singer_name)
             else:
-                Flag = False
+                has_phonemes = False
 
-            # there are 6 samples per file
+            # Extracts samples from files. By default there are 6 samples per file.
             for j in range(config.samples_per_file):
-                # randomly get a WINDOW of 128 frames for each file.
-                # each 128 frames is a sample
+                # Randomly extract a WINDOW of 128 frames to make each sample.
                 broken = False
-                voc_idx = np.random.randint(0,len(feats)-config.max_phr_len)
-                
-                feat = feats[voc_idx:voc_idx+config.max_phr_len]
+                start_index = np.random.randint(0, len(feats) - config.max_phr_len)
+                end_index = start_index + config.max_phr_len
+                sample = feats[start_index : end_index]
                 resample_count = 0
-                while feat.max()>1.0 or feat.min()<0.0:
+
+                # Resample if features are outside the allowed range.
+                while sample.max() > 1.0 or sample.min() < 0.0:
                     resample_count += 1
-                    voc_idx = np.random.randint(0,len(feats)-config.max_phr_len)
-                    feat = feats[voc_idx:voc_idx+config.max_phr_len]
+                    start_index = np.random.randint(0, len(feats) - config.max_phr_len)
+                    end_index = start_index + config.max_phr_len
+                    sample = feats[start_index : end_index]
+                    # To prevent infinite resampling, skip this file if it has tried to resample over 20 times.
                     if resample_count > 20:
                         broken = True
-                        # print(singer_name)
                         break
                 
                 if broken:
                     break
-                
-                targets_f0_1.append(f0_nor[voc_idx:voc_idx+config.max_phr_len])
-                if Flag:
-                    pho_targs.append(pho_target[voc_idx:voc_idx+config.max_phr_len])
-                    targets_singers.append(singer_index)
 
-                assert feat.max()<=1.0 and feat.min()>=0.0, "chicken nigget" 
-                feats_targs.append(feat)
+                target_f0.append(f0_normalized[start_index : end_index])
 
-                if len(feats_targs) == config.batch_size:
+                if has_phonemes:
+                    phonemes.append(pho_target[start_index : end_index])
+                    singers.append(singer_index)
+
+                features.append(sample)
+
+                if len(features) == config.batch_size:
                     break
 
-            # print(len(feats_targs))
+        target_f0 = np.expand_dims(np.array(target_f0), -1)
 
-        # print(len(feats_targs))
-                        
+        features = np.array(features)
+        # if features.max() > 1.0 or features.min() < 0.0:
+        #     continue
 
-        targets_f0_1 = np.expand_dims(np.array(targets_f0_1), -1)
+        assert (
+            features.max() <= 1.0 and features.min() >= 0.0
+        ), "offending singers: " + str(singers)
 
-        feats_targs = np.array(feats_targs)
-        if feats_targs.max()>1.0 or feats_targs.min()<0.0:
-                    continue
-        assert feats_targs.max()<=1.0 and feats_targs.min()>=0.0, "offending singers: " + str(targets_singers) 
-
-        yield feats_targs, targets_f0_1, np.array(pho_targs), np.array(targets_singers) 
+        yield features, target_f0, np.array(phonemes), np.array(singers)
