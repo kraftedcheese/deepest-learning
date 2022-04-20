dc27a9b2f1dc9f6ec1c8ec1873ffec6d0b03182b
kraftedcheese <38340979+kraftedcheese@users.noreply.github.com>
2022-04-20 14:19:56 +0800

    Move conversion to main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from model.wgan_model import WGANModel
 import config
 import os
-import sys
 import argparse
 
 def main(args):
+    #TODO: adjust this
     voc_list = [x for x in os.listdir(config.voice_dir) if 
     x.endswith('.hdf5') and x.startswith('nus') and 
     not x == 'nus_MCUR_sing_04.hdf5' and 
@@ -13,18 +13,26 @@ def main(args):
     not x == 'nus_JTAN_read_07.hdf5']
 
     model = WGANModel(voc_list, args.reload_model)
-    model.train()
-
-    # start evaluating on test data
-    # else:
-    #     model.evaluate(test_loader, args.load_D, args.load_G)
-    #     for i in range(50):
-    #        model.generate_latent_walk(i)
-
+    
+    if args.eval == 'y':
+        if args.reload_model == 0:
+            print("No checkpoint loaded! To evaluate, run main.py --eval y --reload_model \{checkpoint_number\}")
+            return
+        model.test_file_hdf5(args.source, args.target)
+    else:
+        model.train()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
+    # For both training and evaluation, pass in --reload_model {checkpoint} to load.
+    # The checkpoint file MUST be in model_save_dir.
     parser.add_argument('--reload_model', type=int, default='0')
+
+    # Flag for evaluation, pass in --eval {y}
+    parser.add_argument('--eval', type=str, default='n')
+    parser.add_argument('--source', type=str, default='nus_ADIZ_sing_01.hdf5')
+    parser.add_argument('--target', type=str, default='ADIZ')
     args = parser.parse_args()
     main(args)
 
