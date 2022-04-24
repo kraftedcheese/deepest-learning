3267feeb43a6b7114e998bf84a90d1d75a08bdf5
jeanmah <jeanmah.828@gmail.com>
2022-04-24 17:11:46 +0800

    clean up plots

--- plot_histories_graph.py
+++ plot_histories_graph.py
@@ -28,7 +28,7 @@ def plot_loss_graphs(args):
     save_graph(train_df, config.DISCRIMINATOR_LOSS_KEY)
 
     # Plot for W Distance
-    save_graph(train_df, config.W_D_KEY, should_negate = False)    
+    save_graph(train_df, config.W_D_KEY)    
 
     # Plot for G_LOSS
     save_graph(train_df, config.G_LOSS_KEY, y_tick_steps=5)
@@ -36,16 +36,30 @@ def plot_loss_graphs(args):
     # Plot for VAL_LOSS
     save_graph(val_df, config.VAL_LOSS_KEY)
 
+    save_together_discriminator_generator_loss(train_df)
+
     print("Graphs saved to:", config.graph_dir)
 
-def save_graph(df, y_axis_key, should_negate=True, y_tick_steps=1):
+def save_graph(df, y_axis_key, y_tick_steps=1):
     graph_path = os.path.join(config.graph_dir, "{}_graph.png".format(y_axis_key))
-    if should_negate:
-        df[y_axis_key] = df[y_axis_key].apply(lambda x: abs(x))
     y_ticks = np.arange(min(df[y_axis_key]), max(df[y_axis_key]), y_tick_steps)
     x_ticks = np.arange(min(df[config.EPOCH_KEY]), max(df[config.EPOCH_KEY]), 100)
     title = f"{y_axis_key} against {config.EPOCH_KEY}"
     df.plot(x=config.EPOCH_KEY,y=y_axis_key, ylabel = y_axis_key, xlabel = config.EPOCH_KEY, title = title, yticks=y_ticks, xticks=x_ticks)
+    plt.savefig(graph_path)
+
+def save_together_discriminator_generator_loss(df):
+    graph_path = os.path.join(config.graph_dir, "{}_{}_graph.png".format(config.DISCRIMINATOR_LOSS_KEY, config.G_LOSS_KEY))
+    plt.clf()
+    y_ticks = np.arange(min(df[config.G_LOSS_KEY]), max(df[config.G_LOSS_KEY]), 5)
+    x_ticks = np.arange(min(df[config.EPOCH_KEY]), max(df[config.EPOCH_KEY]), 100)
+    plt.yticks(y_ticks)
+    plt.xticks(x_ticks)
+    plt.plot(df[config.DISCRIMINATOR_LOSS_KEY])
+    plt.plot(df[config.G_LOSS_KEY])
+    plt.ylabel('Loss')
+    plt.xlabel(config.EPOCH_KEY)
+    plt.legend([config.DISCRIMINATOR_LOSS_KEY, config.G_LOSS_KEY])
     plt.savefig(graph_path)
 
 
