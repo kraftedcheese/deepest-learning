30ea6fbe249be241dd6b153708347afa9a17dbb7
kraftedcheese <38340979+kraftedcheese@users.noreply.github.com>
2022-04-24 14:41:29 +0800

    Clean up requirements and audio merger

--- audio-merger/overlay_audio.py
+++ audio-merger/overlay_audio.py
@@ -1,17 +1,22 @@
 import glob
 from pydub import AudioSegment, silence
-import os.path 
+import os.path
 import argparse
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--output_name", required=True, help="name of your output file eg. output.mp3")
-parser.add_argument("-d", "--data_folder", required=True, help="path to folder containing wav files eg ./input")
+parser.add_argument(
+    "-o", "--output_name", required=True, help="name of your output file eg. output.mp3"
+)
+parser.add_argument(
+    "-d",
+    "--data_folder",
+    required=True,
+    help="path to folder containing wav files eg ./input",
+)
 args = parser.parse_args()
 
-
-
-
 output_file_name = args.output_name
-data_folder = args.data_folder 
+data_folder = args.data_folder
 
 assert os.path.isdir(data_folder)
 
@@ -23,14 +28,14 @@ largestLead = -9999
 i = 0
 
 # will be used as the base for overlaying later.
-baseIndex = 0 # represents the index of sound file with largest lead silence.
+baseIndex = 0  # represents the index of sound file with largest lead silence.
 for name in names:
     soundInfo = {}
-    sound = AudioSegment.from_file(name, format='wav')
+    sound = AudioSegment.from_file(name, format="wav")
     soundLength = len(sound)
     leadingSil = silence.detect_leading_silence(sound)
     soundInfo["name"] = name
-    soundInfo["soundLength"]  = soundLength
+    soundInfo["soundLength"] = soundLength
     soundInfo["leadingSil"] = leadingSil
     soundInfo["data"] = sound
 
@@ -41,28 +46,26 @@ for name in names:
     i += 1
     listOfSounds.append(soundInfo)
 
-print (listOfSounds)
+print(listOfSounds)
 
-print (f"Largest lead: {largestLead} belongs to {listOfSounds[baseIndex]['name']}")
+print(f"Largest lead: {largestLead} belongs to {listOfSounds[baseIndex]['name']}")
 
 
-# 1) Use largest lead silence as base for overlay 
-output = baseAudio = listOfSounds[baseIndex]['data']
+# 1) Use largest lead silence as base for overlay
+output = baseAudio = listOfSounds[baseIndex]["data"]
 
-# output = listOfSounds[baseIndex]['data']
-
-#2) Overlay with the remaining
+# 2) Overlay with the remaining
 
 for sound in listOfSounds:
     # Skip the base
-    if sound['data'] == baseAudio:
+    if sound["data"] == baseAudio:
         continue
 
     # Overlay the rest
-    output = output.overlay(sound['data'], position = largestLead - sound['leadingSil'])
+    output = output.overlay(sound["data"], position=largestLead - sound["leadingSil"])
 
 if output:
     file_handle = output.export(output_file_name, format="mp3")
-    print ("Merge complete")
-else: 
-    print ("Output is None")
+    print("Merge complete")
+else:
+    print("Output is None")
